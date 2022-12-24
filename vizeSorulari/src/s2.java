a9b3565d6b5a43b0394cc187ffa955c2757e4f4b
Akif <112137968+Soresta@users.noreply.github.com>
2022-12-25 01:22:58 +0300

    Update s2.java

--- vizeSorulari/src/s2.java
+++ vizeSorulari/src/s2.java
@@ -1,6 +1,7 @@
 public class s2 {
     public static void main(String[] args) {
-        // 200 + 1/3 +2/7 + 3/11 ... 30.elemanını ve ilk 50 elemanı yazdıran programı ayzınız!
+        // 200 + 1/3 +2/7 + 3/11 ... diye giden serinin 30. elmanını ekrana yazdıran ve ilk 50 elamnının toplaımını ekrana yazdıran java
+        //kodunu yazınız.
 
         double toplam= 200,payda=3;
 
